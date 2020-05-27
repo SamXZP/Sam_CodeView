106c3c6a5bb8c92fb4c93748346b1b8815b00c49
Sam <gztzxzp@gmail.com>
2020-05-27 14:56:38 +0800

    Initial commit

---
+++ Example/Sam_CodeView/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Sam_CodeView
+//
+//  Created by gztzxzp@163.com on 05/27/2020.
+//  Copyright (c) 2020 gztzxzp@163.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
